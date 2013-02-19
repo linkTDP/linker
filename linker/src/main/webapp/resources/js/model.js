e979575128a4fd028b1df7f7bf1b2956e964593f
Neuro17 <biagio.chirico.86@gmail.com>
2013-02-20 00:29:06 +0100

    0.0.9

--- linker/src/main/webapp/resources/js/model.js
+++ linker/src/main/webapp/resources/js/model.js
@@ -16,8 +16,8 @@ function model(button, div){
   			jq('.alert').append('<strong>Warning!</strong> Best check yo self, you re not looking too good.');				
 		}
 		else{
-			var current=new link(jq('#address').val(), jq('#title').val())
-			var completed=0;
+			var current=new link(jq('#address').val(), jq('#title').val());
+//			var completed=0;
 			jq(function() {
 				// Call a URL and pass two arguments
 				// Also pass a call back function
@@ -29,9 +29,9 @@ function model(button, div){
 							{ 	urll:  current.getAddress(),
 						  		title:  current.getTitle()},
 //						  		readed: current.getRead()},
-//								function(data){
-//									// data contains the result
-//									// Assign result to the sum id
+								function(data){
+									// data contains the result
+									// Assign result to the sum id
 //									completed=data;
 							});
 			});
@@ -52,15 +52,16 @@ function model(button, div){
 }
 
 function linkItem(linkDisplayDiv, link) {
+	var jq = jQuery.noConflict();
 	linkDisplayDiv.append('<div id="linkItem"></div>');
-	$('#linkItem').attr('id', 'linkItem'+ myLink.length);
-	$('#linkItem'+myLink.length).append('<a id="reference" class="ass" target=blank>' + link.getTitle() + '</a>');
-	$('#linkItem'+myLink.length).addClass('item');
-	$('#reference').attr('id', 'reference'+ myLink.length);
-	$('#reference'+ myLink.length).attr('href', link.getAddress());
-	$('#linkItem'+myLink.length).append('<button class="removeItemBtn">delete</button>');
-	$('.removeItemBtn').click(function(){
-		var title = $(this).prev('.ass').text();
+	jq('#linkItem').attr('id', 'linkItem'+ myLink.length);
+	jq('#linkItem'+myLink.length).append('<a id="reference" class="ass" target=blank>' + link.getTitle() + '</a>');
+	jq('#linkItem'+myLink.length).addClass('item');
+	jq('#reference').attr('id', 'reference'+ myLink.length);
+	jq('#reference'+ myLink.length).attr('href', link.getAddress());
+	jq('#linkItem'+myLink.length).append('<button class="removeItemBtn">delete</button>');
+	jq('.removeItemBtn').click(function(){
+		var title = jq(this).prev('.ass').text();
 
 		for(i = 0; i < myLink.length; i++){
 			if(title === myLink[i].getTitle()){
@@ -68,6 +69,6 @@ function linkItem(linkDisplayDiv, link) {
 			}
 		}
 		
-		$(this).parent().remove();	
+		jq(this).parent().remove();	
 	});
 }
